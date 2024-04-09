7cd06c70494e9787f4192b41465820cbe4caf1f7
olfaslimen <98602739+olfaslimen@users.noreply.github.com>
2024-04-09 12:26:37 +0200

    ajouter Routes et Controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("server..");
+const express = require('express');
+const dotenv = require('dotenv').config();
+//import express from "express";
+
+//plus tard** var-env
+const app = express();
+
+const PORT= process.env.PORT || 3000;
+
+//create route with express
+app.get('/',(req,res)=>{
+    res.send('hello');
+})
+
+//Routes
+app.use('/api/users', require("./routes/userRoutes"));
+app.use('/login', require("./routes/userRoutes"));
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
