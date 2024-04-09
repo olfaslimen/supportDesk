8b147e6f172ae534d8f9cff2180c041f7eae5f35
olfaslimen <98602739+olfaslimen@users.noreply.github.com>
2024-04-09 16:51:07 +0200

    login controller install jsonwebtoken package and create a token JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
 
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
+
 
 
 //@desc   Register a new user
@@ -29,18 +30,20 @@ const registerUser =asyncHandler(async(req,res)=>{
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password,salt);
 
-
     //create user
     const user=await User.create({
         name,
         email,
-        password
+        password:hashedPassword
     });
+
+
     if(user){
         res.status(201).json({ 
             _id: user._id,
             name: user.name,
-            email:user.email
+            email:user.email,
+            token: generateToken(user._id)
     })
        
     }else{
@@ -52,10 +55,38 @@ const registerUser =asyncHandler(async(req,res)=>{
 //@desc   Login user
 //@route  /api/users/login
 //@access Public
+
 const loginUser =asyncHandler(async(req,res)=>{
+
+    const {email, password} = req.body;
+    const user= await User.findOne({email})
+
+    //check user and password match
+    
+     //ct= bcrypt.compare(password, user.password);
+    ///res.send(ct);
+    if(user && (await bcrypt.compare(password, user.password)))
+    {
+        //res.send(user);
+        res.status(200).json({ 
+            _id: user._id,
+            name: user.name,
+            email:user.email,
+            token: generateToken(user._id)
+
+    })
+    }else{
+        res.status(401)
+        console.log(password)
+        throw new Error('invalid email or password');
+    }
     res.send('Login Route');
 })
-
+const generateToken = (id)=> {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '60d',
+    })
+}
 module.exports ={
     registerUser,
     loginUser
